2b89d5efe80997987c9cba132d849514337877e1
shivanijadhav1797 <shivanijadhav1797@gmail.com>
2021-03-18 12:51:42 +0530

    Navbar Added

---
+++ jumbocash-frontend/src/components/EntityComponents/ViewEntityComponent.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class ViewEntityComponent extends Component {
+    render() {
+        return (
+            <p>View Entity Component</p>
+        )
+    }
+}
+
+export default ViewEntityComponent
---
+++ jumbocash-frontend/src/components/TransactionComponents/ViewTransactionComponent.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class ViewTransactionComponent extends Component {
+    render() {
+        return (
+            <p>View Transaction Component</p>
+        )
+    }
+}
+
+export default ViewTransactionComponent
