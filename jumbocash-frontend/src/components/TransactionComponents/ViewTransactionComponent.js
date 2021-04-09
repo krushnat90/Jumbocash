560cbec4e8454989a21249bd9cec1748d591d257
shivanijadhav1797 <shivanijadhav1797@gmail.com>
2021-04-09 20:08:35 +0530

    Expand Cell Added in View Transaction

--- jumbocash-frontend/src/components/TransactionComponents/ViewTransactionComponent.js
+++ jumbocash-frontend/src/components/TransactionComponents/ViewTransactionComponent.js
@@ -4,96 +4,21 @@ import { withStyles } from '@material-ui/core/styles';
 import { Spinner } from 'react-bootstrap';
 import {
   DataGrid, GridToolbarContainer,
-  GridToolbarExport
+  GridToolbarExport, isOverflown
 } from '@material-ui/data-grid';
+import PropTypes from 'prop-types';
 import CustomNoRowsOverlay from './CustomNoRowsOverlay'
 import TransactionService from "../../services/TransactionService";
 import Button from '@material-ui/core/Button';
+import Paper from '@material-ui/core/Paper';
+import Popper from '@material-ui/core/Popper';
+import Typography from '@material-ui/core/Typography';
 import EditTwoToneIcon from '@material-ui/icons/EditTwoTone';
 import DeleteTwoToneIcon from '@material-ui/icons/DeleteTwoTone';
 import Modal from '@material-ui/core/Modal';
 import EditTransactionComponent from '../TransactionComponents/EditTransactionComponent'
 
-
-
-// Data grid styling
-const useStyles = theme => ({
-  root: {
-    '& .super-app-theme--header': {
-      backgroundColor: 'rgba(25,31,77, 0.7)',
-      color: 'white',
-      fontWeight: '600'
-    },
-    '& .amount.negative': {
-      color: 'rgb(245,54,92)',
-    },
-    '& .amount.positive': {
-      color: 'rgb(50,207,140)'
-    },
-    '& .tranStatus.done': {
-      color: 'rgb(17,205,239)',
-    },
-    '& .tranStatus.pending': {
-      color: 'rgb(252,146,10)'
-    },
-  },
-});
-
-// Data grid columns
-const columns = [
-  {
-    field: 'tranDate', headerName: 'Date', width: 150,
-    headerClassName: 'super-app-theme--header', headerAlign: 'center', align: 'center'
-  },
-  {
-    field: 'entityName', headerName: 'Entity Name', width: 150,
-    headerClassName: 'super-app-theme--header', headerAlign: 'center', align: 'center'
-  },
-  {
-    field: 'paymentMode', headerName: 'Payment Mode', width: 150,
-    headerClassName: 'super-app-theme--header', headerAlign: 'center', align: 'center'
-  },
-  {
-    field: 'tranType', headerName: 'Type', width: 100,
-    headerClassName: 'super-app-theme--header', headerAlign: 'center', align: 'center',
-    cellClassName: (params) =>
-      clsx('tranType', {
-        credit: params.value === 'credit',
-        debit: params.value === 'debit',
-      }),
-  },
-  {
-    field: 'amount', headerName: 'Amount', width: 150,
-    headerClassName: 'super-app-theme--header', headerAlign: 'center', align: 'center',
-    cellClassName: (params) =>
-      clsx('amount', {
-        positive: params.value > 0,
-        negative: params.value < 0
-      }),
-  },
-  {
-    field: 'tranStatus', headerName: 'Status', width: 100,
-    headerClassName: 'super-app-theme--header', headerAlign: 'center', align: 'center',
-    cellClassName: (params) =>
-      clsx('tranStatus', {
-        pending: params.value === 'Pending',
-        done: params.value === 'Done'
-      }),
-  },
-  {
-    field: 'remarks', headerName: 'Remarks', width: 250,
-    headerClassName: 'super-app-theme--header', align: 'center', headerAlign: 'center'
-  }
-];
-
-// Export to CSV
-function CustomToolbar(transactions) {
-  return (
-    <GridToolbarContainer>
-      <GridToolbarExport />
-    </GridToolbarContainer>
-  );
-}
+import { makeStyles } from "@material-ui/core/styles";
 
 class ViewTransactionComponent extends Component {
 
@@ -230,37 +155,248 @@ class ViewTransactionComponent extends Component {
                 openModal={this.state.openModal}
                 handleClose={this.handleClose}
                 getTransactions={this.getTransactions}
+
               />
             </Modal>
           </div>
           <br />
-          {this.state.message && <div className="alert alert-warning" role="alert">{this.state.message}
-            <button type="button"
-              className="close"
-              data-dismiss="alert"
-              aria-label="Close"
-              onClick={() => this.hideMessageAlert()}>
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>}
-          <DataGrid className={classes.root}
-            components={{
-              NoRowsOverlay: CustomNoRowsOverlay,
-              Toolbar: CustomToolbar,
-            }}
-            onRowSelected={(row) => this.rowSelected(row)}
-            rows={this.state.transactions} columns={columns} sortModel={[
-              {
-                field: 'tranDate',
-                sort: 'desc',
-              },
-            ]}
-          />
+          <div style={{ height: 400, width: '100%' }}>
+            <DataGrid className={classes.root}
+              components={{
+                NoRowsOverlay: CustomNoRowsOverlay,
+                Toolbar: CustomToolbar,
+              }}
+              onRowSelected={(row) => this.rowSelected(row)}
+              rows={this.state.transactions} columns={columns} sortModel={[
+                {
+                  field: 'tranDate',
+                  sort: 'desc',
+                },
+              ]}
+            />
+          </div>
         </div>
       );
     }
   }
 }
+
+// Data grid styling
+const useStyles = theme => ({
+  root: {
+    '& .super-app-theme--header': {
+      backgroundColor: 'rgba(25,31,77, 0.7)',
+      color: 'white',
+      fontWeight: '600'
+    },
+    '& .amount.negative': {
+      color: 'rgb(245,54,92)',
+    },
+    '& .amount.positive': {
+      color: 'rgb(50,207,140)'
+    },
+    '& .tranStatus.done': {
+      color: 'rgb(17,205,239)',
+    },
+    '& .tranStatus.pending': {
+      color: 'rgb(252,146,10)'
+    },
+  },
+});
+
+// Data grid columns
+const columns = [
+  {
+    field: 'tranDate', headerName: 'Date', flex: 1,
+    headerClassName: 'super-app-theme--header', headerAlign: 'center', align: 'center',
+    renderCell: renderCellExpand
+  },
+  {
+    field: 'entityName', headerName: 'Entity Name', flex: 1,
+    headerClassName: 'super-app-theme--header', headerAlign: 'center', align: 'center',
+    renderCell: renderCellExpand
+  },
+  {
+    field: 'paymentMode', headerName: 'Payment Mode', flex: 1,
+    headerClassName: 'super-app-theme--header', headerAlign: 'center', align: 'center',
+    renderCell: renderCellExpand
+  },
+  {
+    field: 'tranType', headerName: 'Type', flex: 1,
+    headerClassName: 'super-app-theme--header', headerAlign: 'center', align: 'center',
+    renderCell: renderCellExpand,
+    cellClassName: (params) =>
+      clsx('tranType', {
+        credit: params.value === 'credit',
+        debit: params.value === 'debit',
+      }),
+  },
+  {
+    field: 'amount', headerName: 'Amount', flex: 1,
+    headerClassName: 'super-app-theme--header', headerAlign: 'center', align: 'center',
+    renderCell: renderCellExpand,
+    cellClassName: (params) =>
+      clsx('amount', {
+        positive: params.value > 0,
+        negative: params.value < 0
+      }),
+  },
+  {
+    field: 'tranStatus', headerName: 'Status', flex: 0.7,
+    headerClassName: 'super-app-theme--header', headerAlign: 'center', align: 'center',
+    renderCell: renderCellExpand,
+    cellClassName: (params) =>
+      clsx('tranStatus', {
+        pending: params.value === 'Pending',
+        done: params.value === 'Done'
+      }),
+  },
+  {
+    field: 'remarks', headerName: 'Remarks', flex: 2,
+    headerClassName: 'super-app-theme--header', align: 'center', headerAlign: 'center',
+    renderCell: renderCellExpand
+  }
+];
+
+
+// Export to CSV
+function CustomToolbar(transactions) {
+  return (
+    <GridToolbarContainer>
+      <GridToolbarExport />
+    </GridToolbarContainer>
+  );
+}
+
+// Cell Expand Styling
+const expandCellStyles = makeStyles(() => ({
+  root: {
+    alignItems: "center",
+    lineHeight: "24px",
+    width: "100%",
+    height: "100%",
+    position: "relative",
+    display: "flex",
+    "& .cellValue": {
+      whiteSpace: "nowrap",
+      overflow: "hidden",
+      textOverflow: "ellipsis"
+    }
+  }
+}));
+
+
+const GridCellExpand = React.memo(function GridCellExpand(props) {
+  const { width, value } = props;
+  const wrapper = React.useRef(null);
+  const cellDiv = React.useRef(null);
+  const cellValue = React.useRef(null);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const classes = expandCellStyles();
+  const [showFullCell, setShowFullCell] = React.useState(false);
+  const [showPopper, setShowPopper] = React.useState(false);
+
+  const handleMouseEnter = () => {
+    const isCurrentlyOverflown = isOverflown(cellValue.current);
+    setShowPopper(isCurrentlyOverflown);
+    setAnchorEl(cellDiv.current);
+    setShowFullCell(true);
+  };
+
+  const handleMouseLeave = () => {
+    setShowFullCell(false);
+  };
+
+  React.useEffect(() => {
+    if (!showFullCell) {
+      return undefined;
+    }
+
+    function handleKeyDown(nativeEvent) {
+      // IE11, Edge (prior to using Bink?) use 'Esc'
+      if (nativeEvent.key === 'Escape' || nativeEvent.key === 'Esc') {
+        setShowFullCell(false);
+      }
+    }
+
+    document.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [setShowFullCell, showFullCell]);
+
+  return (
+    <div
+      ref={wrapper}
+      className={classes.root}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      <div
+        ref={cellDiv}
+        style={{
+          height: 1,
+          width,
+          display: 'block',
+          position: 'absolute',
+          top: 0,
+        }}
+      />
+      <div ref={cellValue} className="cellValue">
+        {value}
+      </div>
+      {showPopper && (
+        <Popper
+          open={showFullCell && anchorEl !== null}
+          anchorEl={anchorEl}
+          style={{ width, marginLeft: -17 }}
+        >
+          <Paper
+            elevation={1}
+            style={{ minHeight: wrapper.current.offsetHeight - 3 }}
+          >
+            <Typography variant="body2" style={{ padding: 8 }}>
+              {value}
+            </Typography>
+          </Paper>
+        </Popper>
+      )}
+    </div>
+  );
+});
+
+GridCellExpand.propTypes = {
+  value: PropTypes.string.isRequired,
+  width: PropTypes.number.isRequired,
+};
+
+function renderCellExpand(params) {
+  return (
+    <GridCellExpand
+      value={params.value ? params.value.toString() : ''}
+      width={params.colDef.width}
+    />
+  );
+}
+
+renderCellExpand.propTypes = {
+  /**
+   * The column of the row that the current cell belongs to.
+   */
+  colDef: PropTypes.any.isRequired,
+  /**
+   * The cell value, but if the column has valueGetter, use getValue.
+   */
+  value: PropTypes.oneOfType([
+    PropTypes.instanceOf(Date),
+    PropTypes.number,
+    PropTypes.object,
+    PropTypes.string,
+    PropTypes.bool,
+  ]),
+};
+
 
 export default withStyles(useStyles)(ViewTransactionComponent);
 
